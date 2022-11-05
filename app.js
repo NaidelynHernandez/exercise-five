c437e0d5a78674bb88d40b4fe877eb7dc21484cf
Naidelyn Hernandez <naidelynhernandez@10-16-156-237.dynapool.wireless.nyu.edu>
2022-11-04 20:34:14 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const express= require('express');
+const app= express();
+const port=4000;
+
+const indexRoute= require('./routes/index.js'); 
+
+app.use('/', indexRoute);
+
+app.listen(port,()=> console.log('working'));
